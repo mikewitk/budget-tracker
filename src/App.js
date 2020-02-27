83bcdfdbd3997df91d14ad98a112d57cae280349
Michael Witkowsky <michael.witk@periscopedev.com>
2020-02-27 18:35:40 -0500

    Add header component and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header/Header'
 
 const App = () => {
   return (
     <div className="app-container">
-      BUDGET TRACKER
+      <Header />
+      <h1>BALANCE COMPONENT</h1>
+      <h1>INCOME/EXPENSE COMPONENT</h1>
+      <h1>TRANSACTIONLIST COMPONENT</h1>
+      <h1>ADD TRANSACTION COMPONENT</h1>
     </div>
   );
 }
