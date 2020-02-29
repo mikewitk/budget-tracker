60c826cb073c7b3d08fcba24de62863afaae1f21
Michael Witkowsky <michael.witk@periscopedev.com>
2020-02-29 10:19:14 -0500

    Change balance styling. Fix general styling

--- src/components/balance/Balance.jsx
+++ src/components/balance/Balance.jsx
@@ -5,18 +5,19 @@ const Balance = () => {
 
   return (
     <div className="balance-container">
+      {/* TODO - balance color based on value */}
       <div className="total-container">
         <h4>YOUR BALANCE</h4>
         <h1>+$50.48</h1>
       </div>
       <div className="income-expense-container">
-        <div className="income-container">
+        <div className="amount-container">
           <h4>INCOME</h4>
-          <p>+$400.73</p>
+          <p className="positive">+$400.73</p>
         </div>
-        <div className="expense-container">
+        <div className="amount-container">
           <h4>EXPENSE</h4>
-          <p className="expense-amount">-$350.25</p>
+          <p className="negative">-$350.25</p>
         </div>
       </div>
     </div>
